c1b33e79998fb1357a06e7cc83619ba38df93ac3
Alecaring <private_a.a.c@icloud.com>
2024-04-03 18:19:10 +0200

    feat: style css, added classes into js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
+"use strict"
+const arrElem = [];
 
-// const arrElem = [];
-
-// // angolo riservato agli elementi push dell'arrey
-// arrElem.push("latte", "uova", "mozzarelle", "pomodori");
-// arrElem.push("cavoli");
-// arrElem.push("latticini");
+// angolo riservato agli elementi push dell'arrey
+arrElem.push("latte", "uova", "mozzarelle", "pomodori");
+arrElem.push("cavoli");
+arrElem.push("latticini");
 
 
-// const conatinerElem = document.getElementById("containerList");
+const conatinerElem = document.getElementById("containerList");
 
-// const ulElem = document.createElement("ul");
-//     conatinerElem.append(ulElem)
-//         console.log(conatinerElem);
+const ulElem = document.createElement("ul");
+ulElem.className = "ulStyle";
+    conatinerElem.append(ulElem);
+        console.log(conatinerElem);
 
-// let i = 0;
-// while (i < arrElem.length) {
+let i = 0;
+while (i < arrElem.length) {
 
-//     const liElem = document.createElement("li");
-//     ulElem.append(liElem);
-//     const Item = arrElem[i];
-//     liElem.append(Item);
+    const liElem = document.createElement("li");
+    liElem.className = "liStyle";
+    ulElem.append(liElem);
+    const Item = arrElem[i];
+    liElem.append(Item);
     
-//     i++;
-// }
-
+    i++;
+}
