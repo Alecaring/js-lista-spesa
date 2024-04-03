37f06231d59ad95a70012601f84f88137623ac39
Alecaring <private_a.a.c@icloud.com>
2024-04-03 18:00:18 +0200

    feat: js/file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 
+// const arrElem = [];
+
+// // angolo riservato agli elementi push dell'arrey
+// arrElem.push("latte", "uova", "mozzarelle", "pomodori");
+// arrElem.push("cavoli");
+// arrElem.push("latticini");
+
+
+// const conatinerElem = document.getElementById("containerList");
+
+// const ulElem = document.createElement("ul");
+//     conatinerElem.append(ulElem)
+//         console.log(conatinerElem);
+
+// let i = 0;
+// while (i < arrElem.length) {
+
+//     const liElem = document.createElement("li");
+//     ulElem.append(liElem);
+//     const Item = arrElem[i];
+//     liElem.append(Item);
+    
+//     i++;
+// }
+
